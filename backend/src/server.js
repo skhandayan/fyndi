3933e0259dd8a0f14197aebd6c9db2a69a9bd430
skhandayan <handayan.seankirk1@gmail.com>
2025-09-27 18:05:04 +0800

    fixing error on backend and frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,35 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/connectDB.js';
-import authRoutes from './routes/auth.route.js';
-import path from 'path'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+// Fix __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname,"../react-frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  // Serve React build
+  app.use(express.static(path.join(__dirname, "../react-frontend/dist")));
 
-  app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-
+  // SPA fallback
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../react-frontend/dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
