1d519a386c2e3dd316b870d149653bdd9d6ede34
skhandayan <handayan.seankirk1@gmail.com>
2025-09-27 14:43:23 +0800

    test

--- react-frontend/vite.config.js
+++ react-frontend/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
@@ -10,5 +10,22 @@ export default defineConfig({
 
   server: {
     host: true,
-  }
-})
+  },
+
+  build: {
+    chunkSizeWarningLimit: 1600, // 📌 increase chunk size limit
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          vendor: [
+            "react-router",
+            "lucide-react",
+            "react-hot-toast",
+          ],
+        },
+      },
+    },
+  },
+});
