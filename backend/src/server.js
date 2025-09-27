0dc9a5e9ffcf4dadd3dc7f7c6e2e597082108fb0
skhandayan <handayan.seankirk1@gmail.com>
2025-09-27 18:12:46 +0800

    fixing error on backend and frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,37 +1,33 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import connectDB from "./config/connectDB.js";
-import authRoutes from "./routes/auth.route.js";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/connectDB.js';
+import authRoutes from './routes/auth.route.js';
+import path from 'path'
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Fix __dirname in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  // Serve static files
-  app.use(express.static(path.join(__dirname, "../react-frontend/dist")));
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,"../react-frontend/dist")))
 
-  // SPA fallback: use a wildcard middleware
   app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, "../react-frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../react-frontend", "dist", "index.html"));
   });
-}
 
+}
 
 app.listen(PORT, () => {
   connectDB();
