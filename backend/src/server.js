908bc886c9ca08108a5fa5502cb1140c3ef98dcf
skhandayan <handayan.seankirk1@gmail.com>
2025-09-27 15:13:55 +0800

    test

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ app.use("/api/auth", authRoutes);
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname,"../react-frontend/dist")))
 
-  app.get("*",(req, res) => {
+  app.get("/*",(req, res) => {
     res.sendFile(path.join(__dirname,"../react-frontend","dist","index.html"))
   })
 }
