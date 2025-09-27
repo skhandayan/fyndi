77acfd157da747a85b0e3b3b88165b99bfca7e05
skhandayan <handayan.seankirk1@gmail.com>
2025-09-27 17:50:45 +0800

    fixing error on backend and frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,51 +1,35 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/connectDB.js';
-import authRoutes from './routes/auth.route.js';
-import path from 'path'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== "production") {
-  // Local development
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-    })
-  );
-} else {
-  // Production
-  app.use(
-    cors({
-      origin: "https://fyndi.onrender.com",
-      credentials: true,
-    })
-  );
-}
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-app.use(express.json());
-app.use(cookieParser())
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname,"../react-frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../react-frontend", "dist", "index.html"));
-  });
-
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 app.listen(PORT, () => {
-  connectDB();
-  console.log("Server is running on port:", PORT);
+	connectDB();
+	console.log("Server is running on port: ", PORT);
 });
