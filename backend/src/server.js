e65e5b0fdc623de466dcbfb213d94966fc1793d7
skhandayan <handayan.seankirk1@gmail.com>
2025-09-27 16:32:52 +0800

    fixing errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,9 +30,10 @@ app.use("/api/auth", authRoutes);
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname,"../react-frontend/dist")))
 
-  app.get("*",(req, res) => {
-    res.sendFile(path.join(__dirname,"../react-frontend","dist","index.html"))
-  })
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../react-frontend", "dist", "index.html"));
+  });
+
 }
 
 app.listen(PORT, () => {
