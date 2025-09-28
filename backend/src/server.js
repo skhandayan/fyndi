64b1b7186161a5f2323953886c923169c9b6364d
skhandayan <handayan.seankirk1@gmail.com>
2025-09-28 08:29:37 +0800

    Deploying

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,8 +13,6 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-import cors from "cors";
-
 app.use(cors({
   origin: process.env.CLIENT_URL, // comes from Railway env
   credentials: true,
