ef559a6f617a2da2645c60a872fbb2840d3339a5
skhandayan <handayan.seankirk1@gmail.com>
2025-09-28 12:14:18 +0800

    Deploying 1

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,30 +4,33 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/connectDB.js';
 import authRoutes from './routes/auth.route.js';
-import path from 'path'
+// import path from 'path' 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve(); // to deploy together frontend and backend
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: process.env.CLIENT_URL, // comes from Railway env
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname,"../react-frontend/dist")))
+// if(process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname,"../react-frontend/dist")))
 
-  app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../react-frontend", "dist", "index.html"));
-  });
+//   app.use((req, res) => {
+//     res.sendFile(path.join(__dirname, "../react-frontend", "dist", "index.html"));
+//   });
 
-}
+// }
 
 app.listen(PORT, () => {
   connectDB();
--- react-frontend/src/store/authStore.js
+++ react-frontend/src/store/authStore.js
@@ -1,7 +1,11 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api/auth"
+    : import.meta.env.VITE_API_URL + "/api/auth";
+
 
 axios.defaults.withCredentials = true;
 
